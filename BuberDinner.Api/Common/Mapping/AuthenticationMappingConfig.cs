68366ac4f55474bc28a68786e3b04f69aa8683f3
PanKunik <patryk.kunicki@pm.me>
2023-09-23 11:46:59 +0200

    Fixing mapping UserId error

--- BuberDinner.Api/Common/Mapping/AuthenticationMappingConfig.cs
+++ BuberDinner.Api/Common/Mapping/AuthenticationMappingConfig.cs
@@ -1,5 +1,6 @@
 using BuberDinner.Application.Authentication.Common;
 using BuberDinner.Contracts.Authentication;
+using BuberDinner.Domain.Users.ValueObjects;
 using Mapster;
 
 namespace BuberDinner.Api.Common.Mapping;
@@ -9,6 +10,8 @@ public class AuthenticationMappingConfig : IRegister
     public void Register(TypeAdapterConfig config)
     {
         config.NewConfig<AuthenticationResult, AuthenticationResponse>()
-            .Map(dest => dest, src => src.User);
+            .Map(dest => dest.Token, src => src.Token)
+            .Map(dest => dest, src => src.User)
+            .Map(dest => dest.Id, src => UserId.Create(src.User.Id.Value));
     }
 }
